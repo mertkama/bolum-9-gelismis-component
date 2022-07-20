9da5d355a8e7dc8272dfaa21005fdd2697effa0b
INVEKOR\Administrator <mert1723@gmail.com>
2022-07-20 15:36:09 +0300

    first commit

---
+++ src/components/NewMemory.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Yeni Anı Componenti....</h1>
+    <button @click="counter++"> Arttır!!!</button>
+    <p> {{ counter }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    data : function(){
+        return {
+            counter : 0
+        }
+    },
+    destroyed(){
+        console.log("Destroyed!!!");
+    },
+    activated(){
+        console.log("Activated!!!")
+    },
+    deactivated(){
+        console.log("DeActivated!!!")
+    }
+  }
+</script>
+
